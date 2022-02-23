5ef084ab96c4adc1dc117ed5dad67a27e96cb1e6
Art <77998829+artdlc20973@users.noreply.github.com>
2022-02-22 19:37:45 -0700

    pics app using unsplash api
    pics app using unsplash api with React 22FEB22

---
+++ precache-manifest.8e234e4214da9d1ca3e212de2a08eb17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec486c39037dd3156fe6add6273f2f17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7befc694c97be2c349b8",
+    "url": "/static/css/main.5678862c.chunk.css"
+  },
+  {
+    "revision": "76dae10225e3351866e2",
+    "url": "/static/js/2.dc787e6e.chunk.js"
+  },
+  {
+    "revision": "7befc694c97be2c349b8",
+    "url": "/static/js/main.a245340c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
